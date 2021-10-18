d9ba20e6edc47260352f8f13a6b21ee7d23487e3
zgarnett <zackgarnett428@gmail.com>
2021-10-18 16:10:59 -0400

    conquered recursion

--- enumerables.rb
+++ enumerables.rb
@@ -21,10 +21,60 @@ class Array
         end
         return select_arr
     end
-            
+    
+    def my_reject(&prc)
+        reject_arr = []
+        i = 0
+        while i < self.length
+            if !prc.call(self[i])
+                reject_arr << self[i]
+            end
+            i += 1
+        end
+        return reject_arr
+    end
+
+    def my_any?(&prc)
+        i = 0
+        while i < self.length
+            return true if prc.call(self[i])
+            i += 1
+        end
+        false
+    end
+
+    def my_all?(&prc)
+        i = 0
+        while i < self.length
+            return false if !prc.call(self[i])
+            i += 1
+        end
+        true
+    end
+
+    def my_flatten
+        flattened = []
+
+        self.each do |ele| 
+            if !ele.is_a?(Array)
+                flattened << ele
+            else
+                var = ele.my_flatten
+                flattened += var
+            end 
+        end
+        flattened
+    end
+    
 end
 
-a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+
+
+
+
+
+
 
